515403856f2ee5bc66e257d520ac226d0affdc95
Callum <callumgoodchild33@gmail.com>
2019-02-02 19:09:38 +0000

    more definitions

--- Haskell Stuff/Test.hs
+++ Haskell Stuff/Test.hs
@@ -1,3 +1,5 @@
 doubleMe x = x + x
 doubleUs x y = x*2 + y*2
-tenMultiplesOf x = take 10 [x*y | y <- [1..10]]
+multiplesOf x = [x*y | y <- [1,2..]]
+tenMultiplesOf x = take 10 (multiplesOf x)
+anyMultiplesOf x z = take z (multiplesOf x)

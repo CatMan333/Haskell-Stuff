2d1a735e849bec9bbad77a76429f4b3a26f66bd2
CatMan333 <36988507+CatMan333@users.noreply.github.com>
2019-02-04 12:13:14 +0000

    Added nCr
    paTri incomplete

--- Haskell Stuff/Test.hs
+++ Haskell Stuff/Test.hs
@@ -3,3 +3,7 @@ doubleUs x y = x*2 + y*2
 multiplesOf x = [x*y | y <- [1,2..]]
 tenMultiplesOf x = take 10 (multiplesOf x)
 anyMultiplesOf x z = take z (multiplesOf x)
+--fctal :: Int -> Int
+fctal x = product[1..x]
+nCr n r = (fctal n)/((fctal r)*(fctal (n-r)))
+paTri = [nCr x y| x<- [1..], y<-[1..]]
